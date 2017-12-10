1d8b48387038bf5fff766cf83836d012d479432b
Courtney Brothers <cournteywbrothers@gmail.com>
2017-12-10 06:19:35 -0600

    output in the dom the sum of the resulting numbers

--- chaining.js
+++ chaining.js
@@ -1,4 +1,7 @@
 let integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-let sorted = integers.sort(function(a, b){return b-a}).filter(number => number < 20);
+let sorted = integers.sort(function(a, b){return b-a}).filter(number => number < 20).map(x => (x * 1.5)-1).reduce(function(prev, curr){return prev + curr;});
 console.log(sorted);
+
+output = document.getElementById("output");
+output.innerHTML = sorted;
